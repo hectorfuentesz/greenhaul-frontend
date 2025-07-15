71dd9202973003703258b656c6306e486161462f
hectorfuentesz <hector.alejandrofz@gmail.com>
2025-07-14 22:48:51 -0600

    ssss

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,7 @@ function showNotification(message, type = 'success') {
     const svgError = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="4"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
     let iconSVGContent = svgSuccess;
     if (type === 'error') iconSVGContent = svgError;
-    if (messageElement) messageElement.tex
-    tContent = message;
+    if (messageElement) messageElement.textContent = message;
     if (iconElement) iconElement.innerHTML = iconSVGContent;
     notificationElement.className = 'cart-notification';
     notificationElement.classList.add(type, 'visible');
